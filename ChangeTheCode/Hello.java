5f35231a21314b22feb10d794c4fe36229f4c595
Matt Hurt <mhurt@msudenver.edu>
2020-08-31 11:49:18 -0600

    fixed issue with extra class statement being added

--- ChangeTheCode/Hello.java
+++ ChangeTheCode/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-public class Hello {
+
     public static void main(final String[] args) {
         //System.out.println("Hello World!");
         System.out.println("Open source is awesome!!!");
