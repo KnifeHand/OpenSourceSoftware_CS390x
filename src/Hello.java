18146f4341adf7f818c47ac2d2443423562ff5f8
Matt Hurt <mhurt@msudenver.edu>
2020-08-31 11:09:08 -0600

    Update projects

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Hello {
+    public static void main(final String[] args) {
+        //System.out.println("Hello World!");
+        System.out.println("Open source is awesome!!!");
+    }
+}
