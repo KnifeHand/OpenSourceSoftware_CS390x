a1308e702c99280198ed8b99bb255e840296dc39
Matt Hurt <mhurt@msudenver.edu>
2020-08-31 11:52:45 -0600

    add line comment about assignment

--- ChangeTheCode/Hello.java
+++ ChangeTheCode/Hello.java
@@ -1,3 +1,17 @@
+/*
+2.3.1. Exercise - Change the source code
+Change the source code to print out "Open source is awesome!!!"
+instead of "Hello World!". Spend no more than half an hour on this exercise.
+
+//Here's an example of source code, in the Java programming language,
+//for a simple, but complete, program.
+public class Hello {
+  public static void main(final String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+ */
+
 public class Hello {
 
     public static void main(final String[] args) {
